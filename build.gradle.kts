6e1253831595597e60bb641e449554fd5d654546
Mariana <marianaramosquiroz@hotmail.com>
2025-02-20 10:28:18 -0500

    Primer commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+
+group = "co.edu.uniquindio"
+version = "0.0.1-SNAPSHOT"
+description = "Breve descripción de la aplicación"
+
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/co/edu/uniquindio/proyectoFinalAvanzada/controllers/SaludoController.java
@@ -0,0 +1,27 @@
+package co.edu.uniquindio.proyectoFinalAvanzada.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/saludo") // Prefijo común para todas las rutas del controlador
+public class SaludoController {
+
+
+    @GetMapping
+    public String saludar(){
+        return "Hola, bienvenido a la aplicación";
+    }
+
+
+    @GetMapping("/{nombre}")
+    public String saludarNombre(@PathVariable String nombre){
+        return "Hola %s, bienvenido a la aplicación".formatted(nombre);
+    }
+
+
+}
+
