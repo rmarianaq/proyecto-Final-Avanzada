2fe97239e765cd7d1fcb4a991e50618fa625bdab
rmarianaq <115004613+rmarianaq@users.noreply.github.com>
2025-04-07 21:24:41 -0500

    Update UserRepository.java

--- src/main/java/co/edu/uniquindio/proyectoFinalAvanzada/repositories/UserRepository.java
+++ src/main/java/co/edu/uniquindio/proyectoFinalAvanzada/repositories/UserRepository.java
@@ -3,10 +3,12 @@ package co.edu.uniquindio.proyectoFinalAvanzada.repositories;
 import co.edu.uniquindio.proyectoFinalAvanzada.model.documents.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     // Buscar usuario por correo
     @Query("{ 'email' : ?0 }")
