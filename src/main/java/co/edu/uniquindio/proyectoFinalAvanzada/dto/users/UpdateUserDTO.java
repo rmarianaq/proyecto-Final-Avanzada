20a6e0f068ebadd05a68a737e9c15d28f9f5ecb4
Juan Camilo Sanchez Cano <juanc.sanchezc1@uqvirtual.eud.co>
2025-04-12 20:43:18 -0500

    Exception UserServiceTest

--- src/main/java/co/edu/uniquindio/proyectoFinalAvanzada/dto/users/UpdateUserDTO.java
+++ src/main/java/co/edu/uniquindio/proyectoFinalAvanzada/dto/users/UpdateUserDTO.java
@@ -1,17 +1,15 @@
 package co.edu.uniquindio.proyectoFinalAvanzada.dto.users;
 
 import co.edu.uniquindio.proyectoFinalAvanzada.model.enums.Municipality;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.UUID;
 
 public record UpdateUserDTO(
         @NotNull String idUser,
         @NotBlank @Length(max = 100) String name,
         @Length(max = 10) String phone,
         Municipality municipality,
-        @NotBlank @Length(max = 100) String address
-) {
+        @NotBlank @Length(max = 100) String address,
+        String number) {
 }
